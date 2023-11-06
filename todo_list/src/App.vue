a4c700fd381a8011092dd89bbf17a8bc60ede0a8
dilhann <dilhan.nathan@gmail.com>
2023-11-06 10:45:35 +0100

    TD1 - Exercice 9 - ajouter tache

--- todo_list/src/App.vue
+++ todo_list/src/App.vue
@@ -4,11 +4,20 @@ let id = 0;
 const taches = ref([{id:id++, description:"Apprendre Vue", faite:false},
   {id:id++, description:"Finir la SAÉ", faite:false},
   {id:id++, description:"Réviser pour l'interro", faite:false}]);
+let nouvelleTache = ref("");
+function ajouterTache(){
+  if(nouvelleTache.value!=""){
+    taches.value.push({id:id++, description:nouvelleTache.value, faite:false});
+    nouvelleTache.value = "";
+  }
+}
 </script>
 
 <template>
   <div id="wrapper">
     <ul>
+      <input type="texte" v-model="nouvelleTache" placeholder="Ajouter une tâche">
+      <button @click="ajouterTache">Valider</button>
       <li v-for="tache in taches" :key="tache.id">
         <span>{{tache.description}}</span>
       </li>
