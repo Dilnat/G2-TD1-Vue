85f52d2c3d199bc91c7896d693468ff3d58aee78
dilhann <dilhan.nathan@gmail.com>
2023-11-06 10:34:45 +0100

    TD1 - Exercice 8 - liste de taches

--- todo_list/src/App.vue
+++ todo_list/src/App.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
 import {ref} from 'vue';
-let texte =  "un texte qui s'affiche";
-let compteur = ref(7);
-function incremente(){
-  compteur.value++;
-  console.log(compteur.value);
-}
-// function onInput(e) {
-//   compteur.value = e.target.value
-// }
+let id = 0;
+const taches = ref([{id:id++, description:"Apprendre Vue", faite:false},
+  {id:id++, description:"Finir la SAÉ", faite:false},
+  {id:id++, description:"Réviser pour l'interro", faite:false}]);
 </script>
-
 
 <template>
   <div id="wrapper">
-    <h1>TODO liste</h1>
-    <p>{{texte}}</p>
-    <a v-bind:href="texte">Test</a>
-    <button v-on:click="incremente">+</button>
-<!--    <input v-bind:value="compteur" v-on:input="compteur">-->
-    <input v-model="compteur">
+    <ul>
+      <li v-for="tache in taches" :key="tache.id">
+        <span>{{tache.description}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
